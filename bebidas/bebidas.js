230023b1ce77f36aa61096373cecd9170496dc1a
Manelxd <87837730+Manelxd@users.noreply.github.com>
2022-03-06 19:11:54 -0300

    adiciona o index bebidas na pasta beidas para trazer uma maior organização

--- bebidas/bebidas.js
+++ bebidas/bebidas.js
@@ -3,9 +3,8 @@ function outros(){
     document.getElementById("cervejas").style.display = "none"
 
 }
+
 function cervejas(){
     document.getElementById("outros").style.display = "none"
     document.getElementById("cervejas").style.display = "flex"
-    document.getElementById("btn-cervejas").style.display = "absolute"
-
 }
