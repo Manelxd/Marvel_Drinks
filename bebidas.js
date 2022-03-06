cf2724b0a98e4778aa90bb10394be42aae57b5ff
Manelxd <87837730+Manelxd@users.noreply.github.com>
2022-03-06 18:22:43 -0300

    faz modifiçoes no layout principal e adiciona  um cardapio de bebidas

---
+++ bebidas.js
@@ -0,0 +1,11 @@
+function outros(){
+    document.getElementById("outros").style.display = "flex"
+    document.getElementById("cervejas").style.display = "none"
+
+}
+function cervejas(){
+    document.getElementById("outros").style.display = "none"
+    document.getElementById("cervejas").style.display = "flex"
+    document.getElementById("btn-cervejas").style.display = "absolute"
+
+}
